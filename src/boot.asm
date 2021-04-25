6f23e0f14e4ce92aea85e7aff8e51c2b52ee4325
Akuna Akwei <akunaakwei@outlook.com>
2021-04-25 21:24:07 +0200

    Some refactoring of the bootloader

--- src/boot.asm
+++ src/boot.asm
@@ -1,23 +1,9 @@
 [bits 16]
 [org 0x7c00]
 
-; set interrupt flag
-sti
-
-; get a20 line status
-mov ax, 0x2402
-int 0x1
-
-; goto start if activated
-cmp al, 1
-jz _start
-
-; enable a20 line
-mov ax, 0x2401
-int 0x15
-
 _start:
 	cli ; clear interrupt flag
+	mov [drive_number], dl ; safe the drive number (loaded by bios into dl) somewhere
 
 	; setup segment registers
 	mov ax, 0
@@ -25,27 +11,64 @@ _start:
 	mov ds, ax ; data segment
 	mov es, ax ; extra segment
 
+	; place stack pointer somewhere
+	mov sp, [_end]
+
+	; read kernel into memory
+	; TODO
+
+	mov dl, [drive_number]
+
+	lidt [idt] ; setup interrupt descriptor table
 	lgdt [gdt] ; setup global descriptor table
-	; prepare protected mode
-	mov eax, cr0
-	or eax, 1
-	mov cr0, eax
-	jmp far_jump
 
-
-
-[bits 32]
-far_jump:
 	; segment registers need to be reloaded after setting gdt up
 	mov ax, 0
 	mov ss, ax ; stack segment
 	mov ds, ax ; data segment
 	mov es, ax ; extra segment
 
+	call a20_line ; activate a20 line
+	jmp kernel_jump ; jump into kernel
 
-	jmp $
+; activate a20 line
+a20_line:
+	sti ; set interrupt flag
+
+	; get a20 line status
+	mov ax, 0x2402
+	int 0x15
+
+	; ret if activated
+	cmp al, 1
+	jz .done
+
+.enable:
+	; enable a20 line
+	mov ax, 0x2401
+	int 0x15
+	; setting flag failed, recover by trying again
+	jnc .enable
+.done:
+	cli ; clear interrupt flag
+	ret
+
+[bits 32]
+
+; because of 32 bit mode we can access eax
+kernel_jump:
+	; prepare protected mode, cr0 flag is needed for that
+	mov eax, cr0 ; move cr0 into a register
+	or eax, 1    ; set it to one
+	mov cr0, eax ; move it back
+
+	; jump into kernel at 0x1000
+	mov eax, 0x1000
+	jmp [eax]
 
 [bits 16]
+; just imagine this is like a data section
+
 ; global descriptor table
 gdt:
 	dw gdt_end - gdt_start - 1
@@ -56,5 +79,16 @@ gdt_null:
 	dq 0
 gdt_end:
 
-times 510-($-$$) db 0x00
-dw 0xAA55
+; interrupt descriptor table
+idt:
+	dw 0
+	dw 0
+	dw 0
+
+; some space to safe the drive number
+drive_number:
+	dw 0
+
+_end:
+	times 510-($-$$) db 0x00
+	dw 0xAA55
